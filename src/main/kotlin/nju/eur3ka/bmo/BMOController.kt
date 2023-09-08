d248d14a2ec072881cd99bb16efffa0605dc8dd9
Eur3ka <njuscimenphy@gmail.com>
2023-09-08 14:34:58 +0800

    new:UI构建

--- src/main/kotlin/nju/eur3ka/bmo/BMOController.kt
+++ src/main/kotlin/nju/eur3ka/bmo/BMOController.kt
@@ -1,22 +1,56 @@
 package nju.eur3ka.bmo
 
+import javafx.geometry.Orientation
 import javafx.scene.control.Button
-import javafx.scene.control.TableColumn
+import javafx.scene.control.ListView
+import javafx.scene.control.Separator
 import javafx.scene.control.TableView
+import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
 import javafx.scene.control.ToolBar
-import javafx.scene.control.TreeItem
-import javafx.scene.control.TreeView
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
 
-object BMOController {
+class BMOController(i18nTransform: TextTransform) {
+    // === main view ===
+    // rootVBox
+    //    \--- mainToolBar [ profileBtn settingBtn ]
+    //    \--- mainHBox
+    //            \--- modVBox
+    //                    \--- modToolBar [ modSavesBtn modBackupBtn modSearchTF modSearchBtn modToolBarSeparator modActivatedNT modNT]
+    //                    \--- modTableView
+    //            \--- archiveVBox
+    //                    \--- archiveToolBar [ archiveNT ]
+    //                    \--- archiveListView
+    //    \--- logTA
     val rootVBox = VBox()
     val mainToolBar = ToolBar()
-    val profileBtn = Button(BMOApp.i18nTransform.transform("button.profile"))
-    val settingBtn = Button(BMOApp.i18nTransform.transform("button.setting"))
+    val profileBtn = Button(i18nTransform.transform("button.profile"))
+    val settingBtn = Button(i18nTransform.transform("button.setting"))
     val mainHBox = HBox()
     val modVBox = VBox()
     val modToolBar = ToolBar()
+    val modSavesBtn = Button(i18nTransform.transform("button.saves"))
+    val modBackupBtn = Button(i18nTransform.transform("button.backup"))
+    val modSearchTF = TextField()
+    val modSearchBtn = Button(i18nTransform.transform("button.search"))
+    val modToolBarSeparator = Separator(Orientation.HORIZONTAL)
+    val modActivatedNT = NumTracker(i18nTransform.transform("numtracker.modactivated"))
+    val modNT = NumTracker(i18nTransform.transform("numtracker.mod"))
+    val modTableView = TableView<ModEntry>()
+    val archiveVBox = VBox()
+    val archiveToolBar = ToolBar()
+    val archiveNT = NumTracker(i18nTransform.transform("numtracker.archive"))
+    val archiveListView = ListView<ModEntry>()
+    val logTA = TextArea().apply { isEditable = false }
 
-    var modArchiveItemTreeView = TreeView<String>()
+    // === mod install view ===
+    val modArchiveInstallView = HBox()
+    val modArchiveInfoVBox = VBox()
+    val modArchiveOperationHBox = HBox()
+    val modArchiveNameTF = TextField().apply { prefWidth = 200.0 }
+    val modArchiveInstallBtn = Button(i18nTransform.transform("button.modarchive.install"))
+    val modArchiveCancelBtn = Button(i18nTransform.transform("button.modarchive.cancel"))
+    val modArchiveInfoTA = TextArea().apply { isEditable = false }
+    var modArchiveTreeView = VBox() // mod 压缩包文件树显示
 }
--- src/main/kotlin/nju/eur3ka/bmo/Main.kt
+++ src/main/kotlin/nju/eur3ka/bmo/Main.kt
@@ -2,17 +2,53 @@ package nju.eur3ka.bmo
 
 import ink.bluecloud.css.CssResources
 import javafx.application.Application
+import javafx.geometry.Orientation
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-object BMOApp : Application() {
+class BMOApp : Application() {
     val appName = "BlueModOrganizer"
     val version = "0.0.1-alpha"
-    var i18nTransform = TextTransform(I18nType.ZH_CN)
+    var i18nTransform = TextTransform(I18nType.zh_CN)
+    private val controller = BMOController(i18nTransform)
+    private fun ui() {
+        controller.rootVBox.run {
+            children.addAll(controller.mainToolBar, controller.mainHBox, controller.logTA)
+        }
+        controller.mainToolBar.run {
+            orientation = Orientation.HORIZONTAL
+            items.addAll(controller.profileBtn, controller.settingBtn)
+        }
+        controller.mainHBox.run {
+            children.addAll(controller.modVBox, controller.archiveVBox)
+        }
+        controller.modVBox.run {
+            children.addAll(controller.modToolBar, controller.modTableView)
+        }
+        controller.modToolBar.run {
+            items.addAll(
+                controller.modSavesBtn,
+                controller.modBackupBtn,
+                controller.modSearchTF,
+                controller.modSearchBtn,
+                controller.modToolBarSeparator,
+                controller.modActivatedNT,
+                controller.modNT
+            )
+        }
+        controller.archiveVBox.run {
+            children.addAll(controller.archiveToolBar, controller.archiveListView)
+        }
+        controller.archiveToolBar.run {
+            items.addAll(controller.archiveNT)
+        }
+    }
+
     override fun start(stage: Stage) {
+        ui()
         stage.run {
-            title = appName
-            scene = Scene(BMOController.rootVBox).apply {
+            title = appName + " " + version
+            scene = Scene(controller.rootVBox).apply {
                 stylesheets.add(CssResources.globalCssFile)
             }
             show()
